07e7435fbdd66881d186e6e5358080c8f9cedd37
Aakashdeep Sil <aakashdeep.work@gmail.com>
2024-09-14 17:21:14 -0500

    index page updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,42 +3,24 @@ import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 import { Container, Title, Button, Grid, Link, Text } from '@components';
-import styles from '@styles/Home.module.css';
 
 const Home = (): JSX.Element => (
   <Container>
-    <Container
-      justifyContent="center"
-      alignContent="center"
-      alignItems="center"
-      textAlign="center"
-      paddingY="25px"
-      paddingBottom="40px"
-      gridGap="4rem"
-    >
+    <Container justifyContent="center" alignContent="center" alignItems="center" textAlign="center" paddingBottom="40px">
       <Container alignItems="center" alignContent="center">
-        <Image
-          src=""
-          alt=""
-          width={120}
-          height={120}
-          objectFit="cover"
-          className={styles.image}
-        />
+        <Image src="/img/profile.png" alt="A cool picture of Aakashdeep Sil" width={180} height={320} />
         <Title>Aakashdeep Sil</Title>
-        <Title
-          fontSize="2rem"
-          color="rgba(0, 0, 0, 0.6)"
-          fontWeight="500"
-          as="h2"
-        >
-          Python | Golang | Java | AWS | Azure | GCP | Kubernetes | Docker | Jenkins | Terraform | Ansible | Git | Linux
+        <Title fontSize="2rem" color="rgba(0, 0, 0, 0.6)" fontWeight="500" as="h2">
+          Ex - Member of Technical Staff @ Oracle
+        </Title>
+        <Title fontSize="1.5rem" color="rgba(0, 0, 0, 0.6)" fontWeight="500" as="h2">
+          Python | Golang | Java | AWS | Azure | Kubernetes | Docker | High-Performance Computing | Jenkins | Terraform | Ansible | Linux | Slurm
         </Title>
       </Container>
-      <Container maxWidth="700px" gridGap="3rem">
+      <Container maxWidth="700px" gridGap="2rem">
         <Container>
           <Text textAlign="center">
-            I&apos;m a Software Engineer with expertise in Cloud, DevOps, SRE, and Software Development
+            I&apos;m a Software Engineer with expertise in Cloud, DevOps, Site Reliability, Backend and Flutter Mobile Application Development.
           </Text>
         </Container>
         <Link href="/about">
@@ -53,27 +35,12 @@ const Home = (): JSX.Element => (
           Get in touch
         </Title>
         <Text textAlign="center">
-          I&apos;m actively looking for job opportunities, my inbox
-          is open for you. Feel free to ask me anything!
+          I&apos;m actively looking for job opportunities in the United States. If you think I can be a good fit for your team, feel free to reach out to me.
         </Text>
-        <Grid
-          gridGap="2rem"
-          marginTop="2rem"
-          gridTemplateColumns={['1fr', 'repeat(1, 1fr)']}
-          justifyItems="stretch"
-          alignItems="stretch"
-        >
+        <Grid gridGap="2rem" marginTop="2rem" gridTemplateColumns={['1fr', 'repeat(1, 1fr)']} justifyItems="stretch" alignItems="stretch">
           <Link href="mailto:aakashdeep.work@gmail.com">
             <Button width="100%">
-              <motion.span
-                initial={{ display: 'inline-block' }}
-                animate={{ rotate: [0, 14, -8, 14, -4, 10, 0, 0] }}
-                transition={{
-                  repeat: Infinity,
-                  repeatType: 'reverse',
-                  duration: 2.5,
-                }}
-              >
+              <motion.span initial={{ display: 'inline-block' }} animate={{ rotate: [0, 14, -8, 14, -4, 10, 0, 0] }} transition={{repeat: Infinity, repeatType: 'reverse', duration: 2.5}}>
                 👋
               </motion.span>{' '}
               Say hello
